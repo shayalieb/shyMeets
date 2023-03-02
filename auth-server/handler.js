92098af4dbe5a64acefb5430b44b90edf5bbf57e
shayalieb <shayalieb@gmail.comgit config --global user.email shayalieb@gmail.comgit config --global user.email shayalieb@gmail.com>
2023-03-02 10:35:20 -0500

    serverless is now working and test auth url is also working

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -15,7 +15,7 @@ const credentials = {
   auth_uri: 'https://accounts.google.com/o/oauth2/auth',
   token_uri: 'https://oauth2.googleapis.com/token',
   auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
-  redirect_uris: ['https://shayalieb.github.io/shyMeets/'],
+  redirect_uris: ['https://shayalieb.github.io/shyMeets'],
   javascript_origins: ['https://shayalieb.github.io', 'http://localhost:3000'],
 };
 
@@ -39,8 +39,11 @@ module.exports.getAuthURL = async () => {
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Credentials': true,
     },
-    body: JSON.stringify({
-      authUrl: authUrl,
-    }),
+    body: authUrl
   };
 };
+
+//getAccessToken for accessing the google calendar
+
+
+//getCalendarEvents 
