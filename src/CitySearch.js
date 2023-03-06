c29ce76fb09707ba638db7dcfc44f9ee5668f14a
shayalieb <shayalieb@gmail.comgit config --global user.email shayalieb@gmail.comgit config --global user.email shayalieb@gmail.com>
2023-03-06 14:15:38 -0500

    up untill i got stuck on test 8

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,15 +1,39 @@
 import React, { Component } from 'react';
+import { mockData } from './mock-data';
+import { extractLocations } from './api';
+
 
 class CitySearch extends Component {
+    state = {
+        query: '',
+        suggestions: []
+    }
+
+    handleInputChange = (event) => {
+        const value = event.target.value;
+        this.setState({ query: value });
+    }
+
     render() {
         return (
             <div className="CitySearch">
                 <input
                     type="text"
                     className="city"
+                    value={this.state.query}
+                    onChange={this.handleInputChange}
                 />
                 <ul className="suggestions">
-                    <li></li>
+                    {this.state.suggestions.map((suggestion) => (
+                        <li
+                            key={suggestion}
+                        >
+                            {suggestion}
+                        </li>
+                    ))}
+                    <li key='all'>
+                        <b>View all cities</b>
+                    </li>
                 </ul>
             </div>
         );
--- src/_tests_/CitySearch.test.js
+++ src/_tests_/CitySearch.test.js
@@ -1,18 +1,44 @@
 import React from "react";
 import { shallow } from "enzyme";
 import CitySearch from "../CitySearch";
+import { mockData } from "../mock-data";
+import { extractLocations } from "../api";
 
 describe('<CitySearch /> component', () => {
-    let CitySearchWrapper;
-    beforeAll(() => {
-        CitySearchWrapper = shallow(<CitySearch />);
+    test('render text input', () => {
+        const CitySearchWrapper = shallow(<CitySearch />)
+        expect(CitySearchWrapper.find('.city')).toHaveLength(1);
+    });
 
-        test('render text input', () => {
-            expect(CitySearchWrapper.find('.city')).toHaveLength(1);
-        })
+    test('render a list of suggestions', () => {
+        const CitySearchWrapper = shallow(<CitySearch />)
+        expect(CitySearchWrapper.find('.suggestions')).toHaveLength(1);
+    });
 
-        test('render a list of suggestions', () => {
-            expect(CitySearchWrapper.find('.suggestions')).toHaveLength(1);
-        })
-    })
-})
+    test('renders text input correctly', () => {
+        const CitySearchWrapper = shallow(<CitySearch />);
+        const query = CitySearchWrapper.state('query');
+        expect(CitySearchWrapper.find('.city').prop('value')).toBe(query);
+    });
+
+    test('change state when text input changes', () => {
+        const CitySearchWrapper = shallow(<CitySearch />);
+        CitySearchWrapper.setState({
+            query: 'Miami'
+        });
+        const eventObject = { target: { value: 'New_York' } };
+        CitySearchWrapper.find('.city').simulate('change', eventObject);
+        expect(CitySearchWrapper.state('query')).toBe('New_York');
+    });
+
+    test('render list of suggestions correctly', () => {
+        const locations = extractLocations(mockData);
+        const CitySearchWrapper = shallow(<CitySearch />);
+        CitySearchWrapper.setState({ suggestions: locations });
+        const suggestions = CitySearchWrapper.state('suggestions');
+        expect(CitySearchWrapper.find('.suggestions li')).toHaveLength(suggestions.length + 1);
+        for (let i = 0; i < suggestions.length; i += 1) {
+            expect(CitySearchWrapper.find('.suggestions li').at(i).text()).toBe(suggestions[i]);
+        }
+    });
+});
