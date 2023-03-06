bef9a12c5ab758003f7192dc00512f5d9e202fd4
shayalieb <shayalieb@gmail.comgit config --global user.email shayalieb@gmail.comgit config --global user.email shayalieb@gmail.com>
2023-03-06 12:36:57 -0500

    stareted the test files

---
+++ src/mock-data.js
@@ -0,0 +1,30 @@
+//This file contains mock data for the purpose of testing
+const mockData = [
+    {
+        "kind": "calendar#event",
+        "etag": "\"3340710207310000\"",
+        "city": "new york",
+    },
+    {
+        "kind": "calendar#event",
+        "etag": "\"3196577121506000\"",
+        "city": "miami",
+    },
+    {
+        "kind": "calendar#event",
+        "etag": "\"3340710199600000\"",
+        "city": "new york",
+    },
+    {
+        "kind": "calendar#event",
+        "etag": "\"3340853146960000\"",
+        "city": "miami",
+    },
+    {
+        "kind": "calendar#event",
+        "etag": "\"3305425136620000\"",
+        "city": "new york",
+    },
+];
+
+export { mockData };
