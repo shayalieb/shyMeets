dcbbcaa76b73a631e0a728772e6f83435cda71ee
shayalieb <shayalieb@gmail.comgit config --global user.email shayalieb@gmail.comgit config --global user.email shayalieb@gmail.com>
2023-03-31 13:02:12 -0400

    with update events error

--- src/App.js
+++ src/App.js
@@ -10,18 +10,8 @@ class App extends Component {
   state = {
     events: [],
     locations: [],
-    numberOfEvents: 15
-  }
-
-  updateEvents = (location) => {
-    getEvents().then((events) => {
-      const locationEvents = (location === 'all') ?
-        events :
-        events.filter((event) => event.location === location);
-      this.setState({
-        events: locationEvents
-      });
-    });
+    numberOfEvents: 32,
+    currentLocations: 'all',
   }
 
   componentDidMount() {
@@ -37,15 +27,71 @@ class App extends Component {
     this.mounted = false;
   }
 
+  updateNoe(number) {
+    this.setState({
+      numberOfEvents: number
+    })
+  }
+
+  updateEvents = (location, eventCount) => {
+    const { currentLocations, numberOfEvents } = this.state;
+    if (location) {
+      getEvents().then((response) => {
+        const locationEvents =
+          location === 'all'
+            ? response.events
+            : response.events.filter((event) => event.location === location);
+        const events = locationEvents.slice(0, numberOfEvents);
+        return this.setState({
+          events: events,
+          currentLocations: location,
+          locations: response.locations
+        });
+      });
+    } else {
+      getEvents().then((response) => {
+        const locationEvents =
+          currentLocations === 'all'
+            ? response.events
+            : response.events.filter((event) => event.location === currentLocations);
+        const events = locationEvents.slice(0, eventCount);
+        return this.setState({
+          events: events,
+          numberOfEvents: eventCount,
+          locations: response.locations
+        });
+      });
+    }
+  };
+
+
+  getData = () => {
+    const { locations, events } = this.state;
+    const data = locations.map((location) => {
+      const number = events.filter((event) => event.location === location).length
+      const city = location.split(' ,').shift()
+      return { city, number }
+    })
+    return data
+  }
+
+
+
   render() {
+    const { numberOfEvents, locations, events } = this.state;
     return (
       <div className='App'>
         <CitySearch
-          locations={this.state.locations}
+          locations={locations}
           updateEvents={this.updateEvents}
         />
-        <EventList events={this.state.events} />
-        <NumberOfEvents />
+        <EventList
+          events={events}
+        />
+        <NumberOfEvents
+          numberOfEvents={numberOfEvents}
+          updateEvents={this.updateEvents}
+        />
       </div>
     )
   }
--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -2,30 +2,43 @@ import React, { Component } from 'react';
 
 class NumberOfEvents extends Component {
     state = {
-        noe: 15
+        noe: 32,
+        errorMessage: ''
     }
 
-    componentDidMount() {
-        this.setState({ noe: this.props.noe || 15 });
-    }
+    // componentDidMount() {
+    //     this.setState({ noe: this.props.noe || 15 });
+    // }
 
-    changeNoe(value) {
-        this.setState({ noe: value });
-        
-    }
+    changeNoe = (event) => {
+        const value = event.target.value;
+        this.props.updateEvents(null, value);
+        this.setState({
+            noe: value
+        })
+        if (value < 1 || value > 32) {
+            this.setState({
+                infoText: 'Select a number from 1 - 32'
+            });
+        } else {
+            this.setState({
+                infoText: ''
+            });
+        }
+    };
 
     render() {
-        const { noe } = this.state
+        const { noe } = this.state;
         return (
             <div className='NumberOfEvents'>
                 <h3>Show Number of Events</h3>
                 <input
+                    id='number-of-events'
                     className='noe-input'
                     type='number'
                     value={noe}
-                    onChange={event => {
-                        this.changeNoe(event.target.value);
-                    }}
+                    onChange={this.changeNoe}
+                    min='0'
                 >
                 </input>
             </div>
