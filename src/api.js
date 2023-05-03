2d2e7bab51877c5fbb2e5c4bf78544952989a77c
shayalieb <shayalieb@gmail.comgit config --global user.email shayalieb@gmail.comgit config --global user.email shayalieb@gmail.com>
2023-05-03 14:03:08 -0400

    added offline functinality

--- src/api.js
+++ src/api.js
@@ -39,13 +39,6 @@ export const getEvents = async () => {
     };
 
     if (!navigator.onLine) {
-        const data = localStorage.getItem("lastEvents");
-        NProgress.done();
-        console.log('offline data: ', data)
-        return data ? JSON.parse(data).events : [];
-    }
-
-    if (!navigator.onLine) {
         const data = localStorage.getItem('lastEvents');
         nProgress.done();
         return data ? JSON.parse(events).events : [];;
