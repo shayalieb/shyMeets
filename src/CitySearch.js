cb4f2530f0bf295be075833a60c981665e67921d
shayalieb <shayalieb@gmail.comgit config --global user.email shayalieb@gmail.comgit config --global user.email shayalieb@gmail.com>
2023-05-03 10:49:05 -0400

    Added a infor alert for CitySearch

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,23 +1,33 @@
 import React, { Component } from 'react';
-
+import { InfoAlert } from './alert';
 
 class CitySearch extends Component {
     state = {
         locations: this.props.locations,
         query: '',
         suggestions: [],
-        showSuggestions: undefined
+        showSuggestions: undefined,
+        infoText: ''
     }
 
     handleInputChanged = (event) => {
         const value = event.target.value;
+        this.setState({ showSuggestions: true });
         const suggestions = this.props.locations.filter((location) => {
             return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
-        })
-        this.setState({
-            query: value,
-            suggestions,
         });
+        if (suggestions.length === 0) {
+            this.setState({
+                query: value,
+                infoText: 'The city you are looking for is not found, Please try again with correct spelling'
+            });
+        } else {
+            return this.setState({
+                query: value,
+                suggestions,
+                infoText: ''
+            })
+        }
     };
 
     handleItemClicked = (suggestion) => {
@@ -39,6 +49,7 @@ class CitySearch extends Component {
                     onChange={this.handleInputChanged}
                     onFocus={() => { this.setState({ showSuggestions: true }) }}
                 />
+                <InfoAlert text={this.state.infoText} />
                 <ul
                     className='suggestions'
                     style={this.state.showSuggestions ? {} : { display: 'none' }}
---
+++ src/alert.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+
+class Alert extends Component {
+    constructor(props) {
+        super(props);
+        this.color = null;//set to null as the children will override this later
+    }
+
+    getStyle = () => {
+        return {
+            color: this.color,
+        };
+    }
+
+    render() {
+        return (
+            <div className="alert">
+                <p style={this.getStyle()}>{this.props.text}</p>
+            </div>
+        );
+    }
+}
+
+class InfoAlert extends Alert {
+    constructor(props) {
+        super(props);
+        this.color = 'blue'
+    }
+}
+
+export { InfoAlert };
