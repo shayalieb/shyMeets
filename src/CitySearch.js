aea61df91268eb6961b1c5f4a5208edf799956f1
shayalieb <shayalieb@gmail.comgit config --global user.email shayalieb@gmail.comgit config --global user.email shayalieb@gmail.com>
2023-05-08 14:57:37 -0400

    Added constructor and super to city search

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,99 +1,16 @@
-// import React, { Component } from "react";
-// //import { getEvents } from "./api";
-// import { InfoAlert } from "./alert";
-
-// class CitySearch extends Component {
-//     constructor() {
-//         super();
-//         this.state = {    
-//             locations: this.props.locations,
-//             query: '',
-//             suggestions: [],
-//             showSuggestions: undefined,
-//             infotext: ''
-//         };
-//     }
-//     handleInputChanged = (event) => {
-//         const value = event.target.value;
-//         this.setState({
-//             showSuggestions: true
-//         });
-
-//         const suggestions = this.props.locations.filter((location) => {
-//             return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
-//         });
-//         if (suggestions.length === 0) {
-//             this.setState({
-//                 query: value,
-//                 infotext: 'The city you are looking for was not found, please try again with correct spelling'
-//             })
-//         } else {
-//             return this.setState({
-//                 query: value,
-//                 suggestions,
-//                 infotext: ''
-//             });
-//         }
-//     }
-
-//     handleItemClicked = (suggestions) => {
-//         this.setState({
-//             query: suggestions,
-//             showSuggestions: false,
-//             infotext: ''
-//         });
-//         this.props.updateEvents(suggestions);
-//     };
-
-//     render() {
-//         return (
-//             <div className="CitySearch">
-//                 <InfoAlert text={this.state.infotext} />
-//                 <input
-//                     type='text'
-//                     className='city'
-//                     value={this.state.query}
-//                     onChange={this.handleInputChanged}
-//                     onFocus={() => {
-//                         this.setState({
-//                             showSuggestions: true
-//                         });
-//                     }}
-//                 />
-//                 <ul
-//                     className='suggestions'
-//                     style={this.state.showSuggestions ? {} : { display: 'none' }}
-//                 >
-//                     {this.state.suggestions.map((suggestion) => (
-//                         <li
-//                             key={suggestion}
-//                             onClick={() => this.handleItemClicked(suggestion)}
-//                         >
-//                             {suggestion}
-//                         </li>
-//                     ))}
-//                     <li onClick={() => this.handleItemClicked('all')}>
-//                         <b>See all cities</b>
-//                     </li>
-//                 </ul>
-//             </div>
-//         );
-//     }
-// }
-
-// export default CitySearch;
-
 import React, { Component } from 'react';
 import { InfoAlert } from './alert';
 
 class CitySearch extends Component {
-    state = {
-        query: '',
-        suggestions: [],
-        showSuggestions: undefined,
-        infoText: ''
+    constructor() {
+        super()
+        this.state = {
+            query: '',
+            suggestions: [],
+            showSuggestions: undefined,
+            infoText: ''
+        }
     }
-
     handleInputChanged = (event) => {
         const value = event.target.value;
         this.setState({
