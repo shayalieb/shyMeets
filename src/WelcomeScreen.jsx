220d3721fe20b0845bd6ff02130198aaa999a6cb
shayalieb <shayalieb@gmail.comgit config --global user.email shayalieb@gmail.comgit config --global user.email shayalieb@gmail.com>
2023-05-11 14:12:14 -0400

    updated welcome screen

--- src/WelcomeScreen.jsx
+++ src/WelcomeScreen.jsx
@@ -1,37 +1,36 @@
-import React from 'react';
-//import { getAccessToken } from './api';
+import React from 'react'
 import './WelcomeScreen.css';
 
 function WelcomeScreen(props) {
-    console.log(props, 'propsError')
-    return props.showWelcomeScreen ? (
-        <div className='WelcomeScreen'>
-            <h1>Welcome to shyMeets App</h1>
-            <h4>
-                Log in to see upcoming events for KolPlay orchestra and more
-            </h4>
-            <div className='button_cont' align="center">
-                <div class="google-btn">
-                    <div class="google-icon-wrapper">
-                        <img
-                            class="google-icon"
-                            src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Log o.svg"
-                            alt="Google sign-in"
-                        />
+    return props.showWelcomeScreen ?
+        (
+            <div className='WelcomeScreen'>
+                <h1>Welcome to shyMeets App</h1>
+                <h4>Login to view all upcoming events for KolPlay Orchestra</h4>
+                <div className='button_cont' align='center'>
+                    <div class='google-btn'>
+                        <div class='google-icon-wrapper'>
+                            <img
+                                class='google-icon'
+                                src='https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Log o.svg'
+                                alt='Google sign-in'
+                            />
+                            <button
+                                onClick={() => { props.getAccessToken }}
+                                rel='nofollow noopener'
+                                class='btn-text'
+                            >
+                                <b>Sign in with Google</b>
+                            </button>
+                        </div>
                     </div>
-                    <button
-                        class="btn-text"
-                        onClick={() => { props.getAccessToken() }}
-                        rel="nofollow noopener"
-                    >
-                        <b>Sign in with Google</b>
-                    </button>
+                    <a href='https://shayalieb.github.io/shyMeets/privacy.html'
+                        rel='nofollow noopener'>
+                        <a href='https://shayalieb.github.io/shyMeets/privacy.html'>Privacy Policy</a>
+                    </a>
                 </div>
             </div>
-            <a href='https://shayalieb.github.io/shyMeets/privacy.html' rel='nofollow noopener'>Privacy Policy</a>
-        </div>
-    )
-        : null
+        ) : null
 }
 
-export default WelcomeScreen;
+export default WelcomeScreen
