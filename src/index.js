abb8b2c7d74ff3fb05cdd4c0ee77c7e79db432ec
shayalieb <shayalieb@gmail.comgit config --global user.email shayalieb@gmail.comgit config --global user.email shayalieb@gmail.com>
2023-05-19 13:51:09 -0400

    registered serviceWorker

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import './index.css';
 import App from './App.js';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import * as serviceWorker from './service-worker';
 import reportWebVitals from './reportWebVitals';
 import { createRoot } from 'react-dom/client';
 
@@ -28,7 +29,7 @@ root.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
 if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator)
-serviceWorkerRegistration.register();
+  serviceWorker.register();
 
 
 // If you want to start measuring performance in your app, pass a function
